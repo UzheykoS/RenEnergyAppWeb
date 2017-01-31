c91485e7381c0a56752bfe031af72ecdbab71582
Sergey Uzheyko <sergio.uzheyko@gmail.com>
2017-01-31 23:16:55 +0200

    Styling

--- src/components/ChartContainer.tsx
+++ src/components/ChartContainer.tsx
@@ -4,6 +4,8 @@ export interface ChartContainerProps { compiler: string; framework: string; }
 
 export class ChartContainer extends React.Component<ChartContainerProps, undefined> {
     render() {
-        return <h1>Hello from me to {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>
+            Hello from me to {this.props.compiler} and {this.props.framework}!
+        </h1>;
     }
 }
