3d77231e8c52e1b255f75cc6bdb024231f04ada4
Sergey Uzheyko <sergio.uzheyko@gmail.com>
2017-01-31 23:27:07 +0200

    Test check out.

--- src/components/ChartContainer.tsx
+++ src/components/ChartContainer.tsx
@@ -5,7 +5,7 @@ export interface ChartContainerProps { compiler: string; framework: string; }
 export class ChartContainer extends React.Component<ChartContainerProps, undefined> {
     render() {
         return <h1>
-            Hello from me to {this.props.compiler} and {this.props.framework}!
+            Hello from me to {this.props.compiler} and {this.props.framework}!!!
         </h1>;
     }
 }
